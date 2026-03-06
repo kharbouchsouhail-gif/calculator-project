bfbd0f7f8a95d79e8e1f96462f9c51c0afe7e8cf
uaser <khabouch.souhail@ensam-casa.ma>
2026-03-06 12:19:01 +0100

    calcul

--- src/CalculatorProject.java
+++ src/CalculatorProject.java
@@ -1,7 +1,54 @@
+import java.util.Scanner;
+
+
 public class CalculatorProject {
     public static void main(String[] args){
 
-        System.out.println("hello");
+       Scanner input = new Scanner(System.in);
+
+       double num1 ;
+       double num2 ;
+       double result = 0;
+
+        System.out.print("enter a operator ( + - / * % ) : \t");
+        String operator = input.nextLine();
+        System.out.println(" \n enter the num 1");
+        num1 = input.nextDouble();
+        System.out.println("enter the num 2");
+        num2 = input.nextDouble();
+
+        switch (operator){
+            case "+" :
+                result = num1 + num2;
+            break;
+            case "-" :
+                result = num1 - num2;
+            break;
+            case "*" :
+                result = num1 * num2;
+            break;
+            case "/" :
+                result = num1 / num2;
+            break;
+            case "%" :
+                result = num1 % num2;
+            break;
+            default:
+                System.out.println("please enter a valid operator");
+
+
+        }
+        System.out.printf("the result is : %f",result);
+
+
+
+
+
+
+
+
+
+       input.close();
 
     }
 
